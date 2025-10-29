ed7a65ca80ae8d569269904aef4c06a1ac84e828
Yaleh Nóbrega <dev.yaleh@gmail.com>
2025-10-29 11:17:31 -0300

    Aula01 de JS.

---
+++ aula01/EntradaSaida.js
@@ -0,0 +1,18 @@
+// Importando a biblioteca
+const leia = require('readline-sync');
+
+// Declarando variaveis
+let nome, distancia;
+
+console.log("Digite o seu nome!!");
+nome = leia.question();
+
+console.log("\nDigite a distancia percorrida em sua corrida");
+distancia = leia.questionFloat();
+
+// Saída de dados
+console.log("\nBom dia, " + nome + ". A distancia percorrida foi de: " + distancia + " km");
+console.log("\nA distancia foi de: " + distancia);
+
+
+
